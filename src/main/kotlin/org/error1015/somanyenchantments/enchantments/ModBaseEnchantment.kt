a6e5a079a6f1702d0176e1b346f84be058f086f3
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-03-17 13:48:53 +0800

    注释

--- src/main/kotlin/org/error1015/somanyenchantments/enchantments/ModBaseEnchantment.kt
+++ src/main/kotlin/org/error1015/somanyenchantments/enchantments/ModBaseEnchantment.kt
@@ -4,6 +4,9 @@ import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.item.enchantment.Enchantment
 import net.minecraft.world.item.enchantment.EnchantmentCategory
 
+/**
+ * 这个东西暂时没有用途
+ */
 abstract class ModBaseEnchantment(pRarity: Rarity, pCategory: EnchantmentCategory, pApplicableSlots: Array<EquipmentSlot>): Enchantment(pRarity, pCategory,pApplicableSlots) {
 
 }
