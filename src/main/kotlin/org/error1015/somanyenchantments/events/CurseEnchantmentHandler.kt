75f2f650dcbc5796d748719f20f7ca7fef6e3ed4
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-03-20 13:58:19 +0800

    补充了密封诅咒的效果概率 目前是20%写死

--- src/main/kotlin/org/error1015/somanyenchantments/events/CurseEnchantmentHandler.kt
+++ src/main/kotlin/org/error1015/somanyenchantments/events/CurseEnchantmentHandler.kt
@@ -61,16 +61,18 @@ object CurseEnchantmentHandler {
             val target = event.entity
             // 如果攻击者的主手物品有密封诅咒，则给目标装备一个随机诅咒
             if (attacker.mainHandItem.isItemEnchanted(SealedCurseEnchantment)) {
-                val armors = listOf(target.helmet, target.chestplate, target.leggings, target.boots) // 目标的护甲List
-                val curses = ForgeRegistries.ENCHANTMENTS.asSequence().filter { it.isCurse }.toList() // 已经注册的诅咒附魔
-                val curseIndex = Random.nextInt(0, curses.size - 1) // 随机诅咒索引
-                val armorsIndex = Random.nextInt(0, armors.size - 1) // 随机护甲索引
-                val armor = armors[armorsIndex] // 随机到的护甲
-                val enchantment = curses[curseIndex] // 随机到的诅咒附魔
-                val level = Random.nextInt(1, enchantment.maxLevel) // 随机到的附魔等级
-                val armorEnchantments = EnchantmentHelper.getEnchantments(armor) // 目标护甲的附魔Map
-                armorEnchantments.plus(enchantment to level) // 添加附魔
-                EnchantmentHelper.setEnchantments(armorEnchantments, armor) // 修改附魔
+                if (Math.random() < 0.2) {
+                    val armors = listOf(target.helmet, target.chestplate, target.leggings, target.boots) // 目标的护甲List
+                    val curses = ForgeRegistries.ENCHANTMENTS.asSequence().filter { it.isCurse }.toList() // 已经注册的诅咒附魔
+                    val curseIndex = Random.nextInt(0, curses.size - 1) // 随机诅咒索引
+                    val armorsIndex = Random.nextInt(0, armors.size - 1) // 随机护甲索引
+                    val armor = armors[armorsIndex] // 随机到的护甲
+                    val enchantment = curses[curseIndex] // 随机到的诅咒附魔
+                    val level = Random.nextInt(1, enchantment.maxLevel) // 随机到的附魔等级
+                    val armorEnchantments = EnchantmentHelper.getEnchantments(armor) // 目标护甲的附魔Map
+                    armorEnchantments.plus(enchantment to level) // 添加附魔
+                    EnchantmentHelper.setEnchantments(armorEnchantments, armor) // 修改附魔
+                }
             }
         }
     }
